eda79c0058e10c3c6967fe537f3ebbd3df8433a8
fuda <memartinenghi@gmail.com>
2024-10-17 17:46:56 -0300

    Add files via upload

--- src/pages/components/navbar.js
+++ src/pages/components/navbar.js
@@ -1,18 +1,20 @@
 import {Container, Nav, Navbar} from 'react-bootstrap';
+import style from "@/styles/Navbar.module.css"
+
 export default function Menu() {
   return (
-    <Navbar expand="lg" className="degradeMenu">
-      <Container>
+  <Container>
+    <Navbar expand="sm" bg="dark" className={`${style.degrademenu} p-2 border border-2 border-primary rounded-2`}>
         <Navbar.Brand href="/">6V</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="/lista">Lista</Nav.Link>
           </Nav>
         </Navbar.Collapse>
-      </Container>
     </Navbar>
+  </Container>
   );
 }
-
--- src/pages/noticias/[id].js
+++ src/pages/noticias/[id].js
@@ -2,5 +2,5 @@ import { useRouter } from "next/router"
 
 export default function Page() {
     const router = useRouter()
-    return <p>Post: {router.query.id}</p>
+    return  <p>Post: {router.query.id}</p>
 }

609d7934447c20f09e85949022b57a1634084b4f
fuda <memartinenghi@gmail.com>
2024-10-29 18:03:28 +0000

    AULA 29/10

--- src/pages/promessa.js
+++ src/pages/promessa.js
@@ -1,14 +1,17 @@
 import { Container } from "react-bootstrap";
 import Menu from "./components/navbar";
 import Footer from "./components/footer";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Promessa(){
     const [divida, setDivida] = useState("")
-    setTimeout(() => {
-        document.title = "Minha Promessa"
-        setDivida(" da vida")
-    }, 2000);
+    useEffect(() => {
+        setTimeout(() => {
+            document.title = "Minha Promessa"
+            setDivida(" da vida")
+        }, 2000);
+    })
+    
     return <>
         <Menu />
         <Container> 
